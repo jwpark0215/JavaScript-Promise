0a7a85c33042d3b4deec78e7ca59c3f8fffc3b6a
jwpark0215 <jwpark021523@outlook.com>
2023-07-02 18:44:07 +1000

    initial practice

---
+++ app.js
@@ -0,0 +1,14 @@
+// console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
+
+const emailRef = document.querySelector(".email");
+console.log(emailRef);
+
+//1. Then 
+fetch("https://jsonplaceholder.typicode.com/users/1")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    emailRef.innerHTML = data.email
+  });
