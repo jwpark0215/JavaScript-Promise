6ea1fa21d2d0888455d775f721f0ccc5d6a10b84
jwpark0215 <jwpark021523@outlook.com>
2023-07-02 19:58:23 +1000

    async/await and then examples for promises

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ const emailRef = document.querySelector(".email");
 console.log(emailRef);
 
 //1. Then 
-fetch("https://jsonplaceholder.typicode.com/users/1")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     emailRef.innerHTML = data.email
+//   });
+
+// 2. Async/Await
+async function main(){
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+    const data = await re sponse.json()
+    console.log(data)
     emailRef.innerHTML = data.email
-  });
+}
+
+main()
+
