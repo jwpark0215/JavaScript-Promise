06b7c030a6c25f14360087bb3d5c384a24a1a05a
jwpark0215 <jwpark021523@outlook.com>
2023-07-02 20:31:17 +1000

    created my own promises example

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
-// console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
+const statusRef = document.querySelector(".status")
+const videoRef = document.querySelector(".video")
 
-const emailRef = document.querySelector(".email");
-console.log(emailRef);
-
-//1. Then 
-// fetch("https://jsonplaceholder.typicode.com/users/1")
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//     emailRef.innerHTML = data.email
-//   });
-
-// 2. Async/Await
-async function main(){
-    const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
-    const data = await re sponse.json()
-    console.log(data)
-    emailRef.innerHTML = data.email
+function getSubscription(){
+    return new Promise((resolve, reject)=>{
+        resolve(undefined)
+    })
 }
 
-main()
+function getVideo(subscriptionStatus){
+    return new Promise((resolve,reject) =>{
+        if(subscriptionStatus === "VIP"){
+            resolve("Show Video")
+        }
+        else if(subscriptionStatus === "FREE"){
+            resolve("show trailer")
+        }
+        else{
+            reject("no video")
+        }
+    })
+ }
 
+
+
+async function letsTry(){
+    const check = (await getSubscription())
+    try{
+        console.log(await getVideo(check))
+    }
+    catch(e){
+         console.log(e)
+         videoRef.innerHTML = e
+    }
+}
+
+letsTry()
